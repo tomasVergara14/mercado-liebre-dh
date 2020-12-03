f33dc8fc889997992eacc9221bbd8b8d249460e7
tomasVergara14 <tomasvergara272@gmail.com>
2020-12-03 10:38:12 -0300

    Pagina de register

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname +"/views/index.html")
+})
+
+app.get("/newsletter", function(req,res){
+    res.sendFile(__dirname + "/views/newsletter.html" )
+})
+
+app.get("/register", function(req,res){
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.listen(3000, function(){
+    console.log("Para ver el sitio ingresar en localhost:3000");
+})
+
+console.log(app);
