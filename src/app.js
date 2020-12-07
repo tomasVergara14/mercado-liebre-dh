f288d02ae4a7836de5eb11dfd951dca1a65b25b4
tomasVergara14 <tomasvergara272@gmail.com>
2020-12-06 21:24:21 -0300

    change

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ app.get("/register", function(req,res){
     res.sendFile(__dirname + "/views/register.html")
 })
 
+app.get("/loggin", function(req,res){
+    res.sendFile(__dirname + "/views/loggin.html")
+})
+
 app.listen(3000, function(){
     console.log("Para ver el sitio ingresar en localhost:3000");
 })
